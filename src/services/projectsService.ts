ab99896c8a8b2f6e88de63f953a70810bd402c27
martinezdap <martinezignaciodev@gmail.com>
2025-07-18 13:24:31 -0300

    Prueba de llamado al back

--- src/services/projectsService.ts
+++ src/services/projectsService.ts
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log('🌐 API_URL:', API_URL);
 
 export async function fetchProjects() {
   const response = await axios.get(`${API_URL}/projects`);
