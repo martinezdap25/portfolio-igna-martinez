f45e2d8e7f5c1f2c3246519e8db550e559bc8c12
martinezdap <martinezignaciodev@gmail.com>
2025-08-18 11:27:12 -0300

    About me - Added interaction to a social media links

--- src/components/about-me/AboutHeader.tsx
+++ src/components/about-me/AboutHeader.tsx
@@ -11,8 +11,10 @@ export default function AboutHeader({ dict }: { dict: Dictionary }) {
         {dict.about.description}
       </p>
       <a
-        href="/cv.pdf"
-        download
+        href="https://drive.google.com/uc?export=download&id=1Gx20zrEn4pUY6rtaQiPrDkH16Py1vyXe"
+        target="_blank"
+        rel="noopener noreferrer"
+        download="Ignacio_Martinez_CV.pdf"
         className="block w-full sm:inline-block sm:w-auto rounded-full bg-indigo-600 text-white px-6 py-2 font-semibold shadow-lg hover:bg-indigo-700 transition-colors duration-300 text-center"
       >
         {dict.about.cv}
