0eb0485aefa1edaa9cce3d44af7f4b0ad05b627a
martinezdap <martinezignaciodev@gmail.com>
2025-08-18 22:59:59 -0300

    Log to resend

--- src/services/projectsService.ts
+++ src/services/projectsService.ts
@@ -2,7 +2,6 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-console.log(API_URL);
 
 export async function fetchProjects(
   filters: {
--- src/services/resend.ts
+++ src/services/resend.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 export const sendEmail = async (firstName: string, email: string) => {
     try {
         const { data } = await axios.post("/api/send", { firstName, email });
+        console.log("Respuesta:", data);
         return data;
     } catch (error) {
         console.error("Error al enviar email:", error);
